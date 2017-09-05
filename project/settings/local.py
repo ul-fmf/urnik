8818e537185add4142448289bbafb4ae8471bb05
Matija Pretnar <matija@pretnar.info>
2017-09-05 10:23:57 +0200

    Premaknil produkcijsko bazo

--- project/settings/local.py
+++ project/settings/local.py
@@ -2,3 +2,13 @@ from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(^hdsfa7b!v0&!pyu2zhd0w($^e&l#qc_q_8dvr)t=ky)fcx87'
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- project/settings/production.py
+++ project/settings/production.py
@@ -10,3 +10,13 @@ ALLOWED_HOSTS = ['tyrion.fmf.uni-lj.si']
 STATIC_URL = '/nov/static/'
 
 LOGIN_URL = '/nov/accounts/login/'
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/srv/urnik/var/db.sqlite3',
+    }
+}
