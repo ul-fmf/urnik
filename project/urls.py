956261b13d0024ffef02b127dda305ad038661c2
Jure Slak <jure.slak@gmail.com>
2019-02-17 19:30:07 +0100

    Posodobil urls.py na novo sintakso in se znebil napak pri login-u in logoutu.

--- project/urls.py
+++ project/urls.py
@@ -14,43 +14,46 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include, url
-from django.contrib import admin, auth
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, re_path, include
+
 import urnik.views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/logout/$', auth.logout, name='logout'),
-    url(r'^accounts/login/$', auth.login, {'template_name': 'admin/login.html'}, name='login'),
-    url(r'^$', urnik.views.zacetna_stran, name='zacetna_stran'),
-    url(r'^urnik/$', urnik.views.kombiniran_pogled, name='kombiniran_pogled'),
-    url(r'^kombiniran/$', urnik.views.kombiniran_pogled_form, name='kombiniran_pogled_form'),
-    url(r'^rezervacije/$', urnik.views.rezervacije, name='rezervacije'),
-    url(r'^nova-rezervacija/$', urnik.views.nova_rezervacija, name='nova_rezervacija'),
-    url(r'^nova-rezervacija/(?P<ucilnica_id>\d+)/(?P<ura>\d+)/(?P<teden>[0-9-]*)/(?P<dan_v_tednu>[0-6])/$', urnik.views.nova_rezervacija, name='nova_rezervacija_za_ucilnico'),
-    url(r'^preglej-rezervacije/$', urnik.views.preglej_rezervacije, name='preglej_rezervacije'),
-    url(r'^potrdi-rezervacijo/$', urnik.views.potrdi_rezervacijo, name='potrdi_rezervacijo'),
-    url(r'^potrdi-vse-rezervacije/$', urnik.views.potrdi_vse_rezervacije, name='potrdi_vse_rezervacije'),
-    url(r'^izbrisi-rezervacijo/$', urnik.views.izbrisi_rezervacijo, name='izbrisi_rezervacijo'),
-    url(r'^proste/$', urnik.views.proste_ucilnice, name='proste'),
-    url(r'^proste_filter/$', urnik.views.proste_ucilnice_filter, name='proste_filter'),
-    url(r'^oseba/(?P<oseba_id>\d+)/$', urnik.views.urnik_osebe, name='urnik_osebe'),
-    url(r'^letnik/(?P<letnik_id>\d+)/$', urnik.views.urnik_letnika, name='urnik_letnika'),
-    url(r'^ucilnica/(?P<ucilnica_id>\d+)/$', urnik.views.urnik_ucilnice, name='urnik_ucilnice'),
-    url(r'^predmet/(?P<predmet_id>\d+)/$', urnik.views.urnik_predmeta, name='urnik_predmeta'),
-    url(r'^srecanje/(?P<srecanje_id>\d+)/premakni/$', urnik.views.premakni_srecanje, name='premakni_srecanje'),
-    url(r'^srecanje/(?P<srecanje_id>\d+)/podvoji/$', urnik.views.podvoji_srecanje, name='podvoji_srecanje'),
-    url(r'^srecanje/(?P<srecanje_id>\d+)/odlozi/$', urnik.views.odlozi_srecanje, name='odlozi_srecanje'),
-    url(r'^srecanje/(?P<srecanje_id>\d+)/trajanje/$', urnik.views.nastavi_trajanje_srecanja, name='nastavi_trajanje_srecanja'),
-    url(r'^preklopi_urejanje/$', urnik.views.preklopi_urejanje, name='preklopi_urejanje'),
-    url(r'^bugreport/$', urnik.views.bug_report, name='bugreport'),
-    url(r'^help/$', urnik.views.help_page, name='help'),
-    url(r'^printall/$', urnik.views.print_all, name='printall'),
-    url(r'^printall/ucilnice/(?P<oddelek>[MF])/$', urnik.views.print_all_ucilnice, name='printall_ucilnice'),
-    url(r'^printall/smeri/(?P<oddelek>[MF])/$', urnik.views.print_all_smeri, name='printall_smeri'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', urnik.views.zacetna_stran, name='zacetna_stran'),
+    path('urnik/', urnik.views.kombiniran_pogled, name='kombiniran_pogled'),
+    path('kombiniran/', urnik.views.kombiniran_pogled_form, name='kombiniran_pogled_form'),
+    path('rezervacije/', urnik.views.rezervacije, name='rezervacije'),
+    path('nova-rezervacija/', urnik.views.nova_rezervacija, name='nova_rezervacija'),
+    re_path(r'^nova-rezervacija/(?P<ucilnica_id>\d+)/(?P<ura>\d+)/(?P<teden>[0-9-]*)/(?P<dan_v_tednu>[0-6])/$',
+            urnik.views.nova_rezervacija, name='nova_rezervacija_za_ucilnico'),
+    path('preglej-rezervacije/', urnik.views.preglej_rezervacije, name='preglej_rezervacije'),
+    path('potrdi-rezervacijo/', urnik.views.potrdi_rezervacijo, name='potrdi_rezervacijo'),
+    path('potrdi-vse-rezervacije/', urnik.views.potrdi_vse_rezervacije, name='potrdi_vse_rezervacije'),
+    path('izbrisi-rezervacijo/', urnik.views.izbrisi_rezervacijo, name='izbrisi_rezervacijo'),
+    path('proste/', urnik.views.proste_ucilnice, name='proste'),
+    path('proste_filter', urnik.views.proste_ucilnice_filter, name='proste_filter'),
+    path('oseba/<int:oseba_id>', urnik.views.urnik_osebe, name='urnik_osebe'),
+    path('letnik/<int:letnik_id>', urnik.views.urnik_letnika, name='urnik_letnika'),
+    path('ucilnica/<int:ucilnica_id>', urnik.views.urnik_ucilnice, name='urnik_ucilnice'),
+    path('predmet/<int:predmet_id>', urnik.views.urnik_predmeta, name='urnik_predmeta'),
+    path('srecanje/<int:srecanje_id>/premakni', urnik.views.premakni_srecanje, name='premakni_srecanje'),
+    path('srecanje/<int:srecanje_id>/podvoji', urnik.views.podvoji_srecanje, name='podvoji_srecanje'),
+    path('srecanje/<int:srecanje_id>/odlozi', urnik.views.odlozi_srecanje, name='odlozi_srecanje'),
+    path('srecanje/<int:srecanje_id>/trajanje', urnik.views.nastavi_trajanje_srecanja, name='nastavi_trajanje_srecanja'),
+    path('preklopi_urejanje', urnik.views.preklopi_urejanje, name='preklopi_urejanje'),
+    path('bugreport/', urnik.views.bug_report, name='bugreport'),
+    path('help/', urnik.views.help_page, name='help'),
+    path('printall/', urnik.views.print_all, name='printall'),
+    re_path(r'^printall/ucilnice/(?P<oddelek>[MF])/$', urnik.views.print_all_ucilnice, name='printall_ucilnice'),
+    re_path(r'^printall/smeri/(?P<oddelek>[MF])/$', urnik.views.print_all_smeri, name='printall_smeri'),
 ]
 
 if 'silk' in settings.INSTALLED_APPS:
     urlpatterns += [
-        url(r'^silk/', include('silk.urls', namespace='silk')),
+        path('silk/', include('silk.urls', namespace='silk')),
     ]
