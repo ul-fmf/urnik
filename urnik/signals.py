ac4caf33f62fa9ed163179619c490a44a4a0b149
Jure Slak <jure.slak@gmail.com>
2019-02-24 19:56:15 +0100

    Link user to oseba.

--- urnik/signals.py
+++ urnik/signals.py
@@ -5,13 +5,22 @@ from urnik.models import Oseba
 
 
 @receiver(populate_user)
-def connect_oseba_to_user(sender,  user=None, ldap_user=None, **kwargs):
+def connect_oseba_to_user(sender,  user =None, ldap_user=None, **kwargs):
     if user is None:
         return
-
+    user.save()
     try:
         user.oseba
         return
     except Oseba.DoesNotExist:
-        # najdemo osebo in povežemo
-        pass
+        if not user.first_name or not user.last_name:
+            return
+        osebe = list(Oseba.objects.filter(ime=user.first_name, priimek=user.last_name, user__isnull=True))
+        if not osebe:
+            oseba = Oseba(ime=user.first_name, priimek=user.last_name)
+        else:
+            oseba = osebe[0]
+
+        oseba.user = user
+        oseba.email = user.email
+        oseba.save()
